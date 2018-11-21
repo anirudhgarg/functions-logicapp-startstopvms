cebadb47ce1a7b39ea3d2bde8f60cd973d535075
Anirudh Garg <anirudhg@microsoft.com>
2018-11-21 15:58:41 -0800

    Adding usage and cleaning up error logging

--- StartStopVMs/StartStopVMs.cs
+++ StartStopVMs/StartStopVMs.cs
@@ -15,6 +15,7 @@ using Microsoft.Azure.Services.AppAuthentication;
 using System.Collections.Generic;
 using Microsoft.Azure.Management.Compute.Fluent;
 using System.Text;
+using System;
 
 namespace StartStopVMs
 {    
@@ -26,7 +27,7 @@ namespace StartStopVMs
         {
             string subscriptionId = req.Query["subscriptionId"];
             string resourceGroupName = req.Query["resourceGroupName"];
-            string tagsToCheck = req.Query["tagsToCheck"];
+            string tag = req.Query["tag"];
             string mode = req.Query["mode"];
             StringBuilder resultText = new StringBuilder();
            
@@ -44,59 +45,67 @@ namespace StartStopVMs
             {
                 resourceGroupName = data["resourceGroupName"];
             }
-            if (data.ContainsKey("tagsToCheck"))
+            if (data.ContainsKey("tag"))
             {
-                tagsToCheck = data["tagsToCheck"];
+                tag = data["tag"];
             }           
 
             if(string.IsNullOrEmpty(subscriptionId) || string.IsNullOrEmpty(mode))
             {
-                return new BadRequestObjectResult("Please make sure subscriptionId and mode are part of the post json body");
+                return new BadRequestObjectResult(@"subscriptionId and mode are required. Usage: {""subscriptionId"":""<subscriptionid>"", ""mode"":""start|stop"", [""resourceGroupName"":""<resourceGroupName>""], [""tag"":""<tag>""]  ");
             }
 
             string resultstring = requestBody.ToString();
-            string token = Authenticate().Result;
-            AzureCredentials credentials = new AzureCredentials(new TokenCredentials(token), new TokenCredentials(token), string.Empty, AzureEnvironment.AzureGlobalCloud);
-            //subscriptionId: 4dda6ad2-730a-4053-88d1-0fa7ff209aea
-            //resourceGroupName: 
-            var azure = Azure.Configure().WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic).Authenticate(credentials).WithSubscription(subscriptionId);
-            
-            var vmList = !string.IsNullOrEmpty(resourceGroupName)
-                ? azure.VirtualMachines.ListByResourceGroup(resourceGroupName)
-                : azure.VirtualMachines.List();
-
-            List<Task> tasks = new List<Task>();
             int numberOfVmsAffected = 0;
-            foreach (var vm in vmList)
-            {                   
-                if ( (!string.IsNullOrEmpty(tagsToCheck) && vm.Tags.ContainsKey(tagsToCheck)) || string.IsNullOrEmpty(tagsToCheck) )
-                {
-                    Task task = null;
-                    if (mode == "start" && (vm.PowerState == PowerState.Stopped || vm.PowerState == PowerState.Deallocated || vm.PowerState == PowerState.Deallocating || vm.PowerState == PowerState.Stopping))
-                    {
-                        log.LogInformation("Starting vm {0}", vm.Name);
-                        resultText.AppendLine(string.Format("Started vm {0}", vm.Name));
-                        numberOfVmsAffected++;
-                        task =  vm.StartAsync();
-                    }
-                    else if (mode == "stop")
-                    {
-                        log.LogInformation("Stopping vm {0}", vm.Name);
-                        resultText.AppendLine(string.Format("Stopped vm {0}", vm.Name));
-                        numberOfVmsAffected++;
-                        task =  vm.DeallocateAsync();
-                    }
-                    if(task != null) tasks.Add(task);
-                }
-            }                    
-           
-            foreach (Task task in tasks)
+            try
             {
-                await task;
+                string token = Authenticate().Result;
+                AzureCredentials credentials = new AzureCredentials(new TokenCredentials(token), new TokenCredentials(token), string.Empty, AzureEnvironment.AzureGlobalCloud);
+                //subscriptionId: 4dda6ad2-730a-4053-88d1-0fa7ff209aea
+                //resourceGroupName: 
+                var azure = Azure.Configure().WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic).Authenticate(credentials).WithSubscription(subscriptionId);
+           
+            
+                var vmList = !string.IsNullOrEmpty(resourceGroupName)
+                    ? azure.VirtualMachines.ListByResourceGroup(resourceGroupName)
+                    : azure.VirtualMachines.List();
+
+                List<Task> tasks = new List<Task>();               
+                foreach (var vm in vmList)
+                {                   
+                    if ( (!string.IsNullOrEmpty(tag) && vm.Tags.ContainsKey(tag)) || string.IsNullOrEmpty(tag) )
+                    {
+                        Task task = null;
+                        if (mode == "start" && (vm.PowerState == PowerState.Stopped || vm.PowerState == PowerState.Deallocated || vm.PowerState == PowerState.Deallocating || vm.PowerState == PowerState.Stopping))
+                        {
+                            log.LogInformation("Starting vm {0}", vm.Name);
+                            resultText.AppendLine(string.Format("Started vm {0}", vm.Name));
+                            numberOfVmsAffected++;
+                            task =  vm.StartAsync();
+                        }
+                        else if (mode == "stop")
+                        {
+                            log.LogInformation("Stopping vm {0}", vm.Name);
+                            resultText.AppendLine(string.Format("Stopped vm {0}", vm.Name));
+                            numberOfVmsAffected++;
+                            task =  vm.DeallocateAsync();
+                        }
+                        if(task != null) tasks.Add(task);
+                    }
+                }                    
+           
+                foreach (Task task in tasks)
+                {
+                    await task;
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(string.Format("Invocation failed with exception: {0}", ex.Message));
             }
 
             resultText.Insert(0, string.Format("Numbers of VM's affected {0}{1}", numberOfVmsAffected, System.Environment.NewLine));
-
             return new OkObjectResult($"{resultText.ToString()}");                
         }
       
